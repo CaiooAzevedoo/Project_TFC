62e1b33a3c5f471d11434fd1162f43a6a0ef9fc8
Caio <caiusito@gmail.com>
2023-02-01 13:36:17 -0300

    Sessão 4 - Requisito 29 e 30

--- app/backend/src/services/LeaderBoard.service.ts
+++ app/backend/src/services/LeaderBoard.service.ts
@@ -11,7 +11,18 @@ const generateBoard = async (homeAway: boolean) => {
 
   const board = leaderBoard.geraQuadro(teams, matchsFinished, homeAway);
 
-  return board;
+  return board.sort((a, b) => {
+    if (b.totalPoints - a.totalPoints !== 0) {
+      return b.totalPoints - a.totalPoints;
+    }
+    if (b.totalVictories - a.totalVictories !== 0) {
+      return b.totalVictories - a.totalVictories;
+    }
+    if (b.goalsBalance - a.goalsBalance !== 0) {
+      return b.goalsBalance - a.goalsBalance;
+    }
+    return b.goalsFavor - a.goalsFavor;
+  });
 };
 
 // const generateBoard = async () => Team.findAll();
