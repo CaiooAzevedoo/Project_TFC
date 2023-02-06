b1277fab92c49fc0ba211b371a90ac699c0de822
Caio <caiusito@gmail.com>
2023-02-06 11:23:44 -0300

    Sesso 4 - Requisito 31

--- app/backend/src/Routes/board.route.ts
+++ app/backend/src/Routes/board.route.ts
@@ -4,5 +4,6 @@ import { boardController } from '../controllers';
 const router = Router();
 
 router.get('/home', boardController.getHomeBoard);
+router.get('/away', boardController.getAwayBoard);
 
 export default router;
--- app/backend/src/controllers/leaderBoard.controller.ts
+++ app/backend/src/controllers/leaderBoard.controller.ts
@@ -7,8 +7,15 @@ const getHomeBoard = async (_req: Request, res: Response) => {
   return res.status(200).json(board);
 };
 
+const getAwayBoard = async (_req: Request, res: Response) => {
+  const board = await boardService.generateBoard(false);
+
+  return res.status(200).json(board);
+};
+
 const boardController = {
   getHomeBoard,
+  getAwayBoard,
 };
 
 export default boardController;
